7a6ba5afbe79922029c4201d0913955577043886
firewolf8385 <Colby@DESKTOP-SIDJGO7>
2018-06-03 16:49:24 -0400

    Added PlaceholderAPI support

--- ElytraPvP/src/firewolf8385/elytrapvp/Main.java
+++ ElytraPvP/src/firewolf8385/elytrapvp/Main.java
@@ -1,5 +1,6 @@
 package firewolf8385.elytrapvp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -8,6 +9,15 @@ public class Main extends JavaPlugin{
 	
 	public void onEnable() {
 		settings.setup(this);
+		
+		// Checks if PlaceholderAPI is installed
+		if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+			Bukkit.getLogger().info("[ElytraPVP] PlaceholderAPI detected! Enabling placeholders...");
+			new Placeholders(this).hook();
+		}
+		else {
+			Bukkit.getLogger().warning("[ElytraPvP] PlaceholderAPI not found. Placeholders will not work without this!");
+		}
 	}
 	
 }
